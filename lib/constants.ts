cc09d4a2d7510e5d7abc8b375c91243d105bc56d
Your Name <you@example.com>
2025-05-29 15:11:52 +0530

    website-layout

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Ecom'
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Your one-stop shop for everything'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'E-commerce B2C BUILT WITH NEXT.JS AND MONFOGO DB'
