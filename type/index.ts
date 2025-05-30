2681e971344d0985d74918b811722bef69f481f6
Your Name <you@example.com>
2025-05-30 18:14:47 +0530

    signup-form

--- type/index.ts
+++ type/index.ts
@@ -4,6 +4,7 @@ import {
   ProductInputSchema,
   UserInputSchema,
   UserSignInSchema,
+  UserSignUpSchema,
 } from '@/lib/validator'
 import { z } from 'zod'
 
@@ -66,3 +67,5 @@ export type Cart = z.infer<typeof CartSchema>
 //user
 export type IUserInput = z.infer<typeof UserInputSchema>
 export type IUserSignIn = z.infer<typeof UserSignInSchema>
+
+export type IUserSignUp = z.infer<typeof UserSignUpSchema>
