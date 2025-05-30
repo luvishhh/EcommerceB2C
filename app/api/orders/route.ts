dfe2d6419bc2df5802c85d6a59bf388398564242
Your Name <you@example.com>
2025-05-31 00:10:30 +0530

    failed-paypal-integration

--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -4,6 +4,9 @@ import { createOrder } from '@/lib/actions/order.action'
 export const runtime = 'nodejs'
 
 export async function POST(req: NextRequest) {
+  console.log('RESEND_API_KEY:', process.env.RESEND_API_KEY)
+  console.log('SENDER_EMAIL:', process.env.SENDER_EMAIL)
+
   const cart = await req.json()
   const result = await createOrder(cart)
   return Response.json(result)
--- lib/actions/order.action.ts
+++ lib/actions/order.action.ts
@@ -7,7 +7,10 @@ import { Cart, OrderItem, ShippingAddress } from '@/type'
 import { connectToDatabase } from '../db'
 import { auth } from '@/auth'
 import { OrderInputSchema } from '../validator'
-import Order from '../db/models/order.model'
+import Order, { IOrder } from '../db/models/order.model'
+import { paypal } from '../paypal'
+import { sendPurchaseReceipt } from '@/emails'
+import { revalidatePath } from 'next/cache'
 
 // CREATE
 export const createOrder = async (clientSideCart: Cart) => {
@@ -33,27 +36,98 @@ export const createOrderFromCart = async (
   clientSideCart: Cart,
   userId: string
 ) => {
-  const cart = {
-    ...clientSideCart,
-    ...calcDeliveryDateAndPrice({
-      items: clientSideCart.items,
-      shippingAddress: clientSideCart.shippingAddress,
-      deliveryDateIndex: clientSideCart.deliveryDateIndex,
-    }),
-  }
+  const calculatedValues = await calcDeliveryDateAndPrice({
+    items: clientSideCart.items,
+    shippingAddress: clientSideCart.shippingAddress,
+    deliveryDateIndex: clientSideCart.deliveryDateIndex,
+  })
 
   const order = OrderInputSchema.parse({
     user: userId,
-    items: cart.items,
-    shippingAddress: cart.shippingAddress,
-    paymentMethod: cart.paymentMethod,
-    itemsPrice: cart.itemsPrice,
-    shippingPrice: cart.shippingPrice,
-    taxPrice: cart.taxPrice,
-    totalPrice: cart.totalPrice,
-    expectedDeliveryDate: cart.expectedDeliveryDate,
+    items: clientSideCart.items,
+    shippingAddress: clientSideCart.shippingAddress,
+    paymentMethod: clientSideCart.paymentMethod || 'PayPal',
+    itemsPrice: calculatedValues.itemsPrice.toFixed(2),
+    shippingPrice: calculatedValues.shippingPrice
+      ? calculatedValues.shippingPrice.toFixed(2)
+      : '0.00',
+    taxPrice: calculatedValues.taxPrice
+      ? calculatedValues.taxPrice.toFixed(2)
+      : '0.00',
+    totalPrice: calculatedValues.totalPrice.toFixed(2),
+    expectedDeliveryDate: calculatedValues.expectedDeliveryDate,
+    isPaid: false,
+    isDelivered: false,
   })
   return await Order.create(order)
+}
+export async function getOrderById(orderId: string): Promise<IOrder> {
+  await connectToDatabase()
+  const order = await Order.findById(orderId)
+  return JSON.parse(JSON.stringify(order))
+}
+
+export async function createPayPalOrder(orderId: string) {
+  await connectToDatabase()
+  try {
+    const order = await Order.findById(orderId)
+    if (order) {
+      const paypalOrder = await paypal.createOrder(order.totalPrice)
+      order.paymentResult = {
+        id: paypalOrder.id,
+        email_address: '',
+        status: '',
+        pricePaid: '0',
+      }
+      await order.save()
+      return {
+        success: true,
+        message: 'PayPal order created successfully',
+        data: paypalOrder.id,
+      }
+    } else {
+      throw new Error('Order not found')
+    }
+  } catch (err) {
+    return { success: false, message: formatError(err) }
+  }
+}
+
+export async function approvePayPalOrder(
+  orderId: string,
+  data: { orderID: string }
+) {
+  await connectToDatabase()
+  try {
+    const order = await Order.findById(orderId).populate('user', 'email')
+    if (!order) throw new Error('Order not found')
+
+    const captureData = await paypal.capturePayment(data.orderID)
+    if (
+      !captureData ||
+      captureData.id !== order.paymentResult?.id ||
+      captureData.status !== 'COMPLETED'
+    )
+      throw new Error('Error in paypal payment')
+    order.isPaid = true
+    order.paidAt = new Date()
+    order.paymentResult = {
+      id: captureData.id,
+      status: captureData.status,
+      email_address: captureData.payer.email_address,
+      pricePaid:
+        captureData.purchase_units[0]?.payments?.captures[0]?.amount?.value,
+    }
+    await order.save()
+    await sendPurchaseReceipt({ order })
+    revalidatePath(`/account/orders/${orderId}`)
+    return {
+      success: true,
+      message: 'Your order has been successfully paid by PayPal',
+    }
+  } catch (err) {
+    return { success: false, message: formatError(err) }
+  }
 }
 export const calcDeliveryDateAndPrice = async ({
   items,
@@ -77,13 +151,19 @@ export const calcDeliveryDateAndPrice = async ({
         : deliveryDateIndex
     ]
 
+  // Calculate expected delivery date
+  const expectedDeliveryDate = new Date()
+  expectedDeliveryDate.setDate(
+    expectedDeliveryDate.getDate() + deliveryDate.daysToDeliver
+  )
+
   const shippingPrice =
     !shippingAddress || !deliveryDate
       ? undefined
       : deliveryDate.freeShippingMinPrice > 0 &&
-        itemsPrice >= deliveryDate.freeShippingMinPrice
-      ? 0
-      : deliveryDate.shippingPrice
+          itemsPrice >= deliveryDate.freeShippingMinPrice
+        ? 0
+        : deliveryDate.shippingPrice
 
   // Calculate tax as 15% of items price
   const taxPrice = round2(itemsPrice * 0.15)
@@ -103,5 +183,6 @@ export const calcDeliveryDateAndPrice = async ({
     shippingPrice,
     taxPrice,
     totalPrice,
+    expectedDeliveryDate,
   }
 }
