1ecd5a26b277041d5e585ea8715e225b5540620c
Your Name <you@example.com>
2025-05-30 17:47:54 +0530

    update-error-01

--- auth.ts
+++ auth.ts
@@ -20,6 +20,7 @@ declare module 'next-auth' {
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   ...authConfig,
+  secret: process.env.AUTH_SECRET,
   pages: {
     signIn: '/sign-in',
     newUser: '/sign-up',
